0188a65ef6af3b5498c283034db5454e99a9daaf
Polina Soshnin <psoshnin@wellesley.edu>
2014-02-22 21:21:47 -0500

    create custom views for user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,4 +37,11 @@ class User
 
   ## Token authenticatable
   # field :authentication_token, :type => String
+
+  #enable users to have names 
+  field :name
+  validates_presence_of :name
+  validates_uniqueness_of :name, :email, :case_sensitive => false
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+
 end
